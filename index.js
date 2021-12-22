2d58da63d562534ef070a20a2b7e4b419524ea1d
Matheus Godoy Landgraf <matheus_godoyland@hotmail.com>
2021-12-22 10:18:03 -0300

    ultima versao

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function pessoal(){
-    window.location = "pessoal.html"
-}
+const nome = document.getElementById('nome')
+const email = document.getElementById('email')
+const mensagem = document.getElementById('mensagem')
+const form = document.getElementById('form')
+
+if ()
