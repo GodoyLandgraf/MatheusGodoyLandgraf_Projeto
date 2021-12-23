21281bb9ee309d0cefc594f127ba029822d21e94
Matheus Godoy Landgraf <matheus_godoyland@hotmail.com>
2021-12-23 11:50:20 -0300

    prontooooo

--- form.js
+++ form.js
@@ -1,27 +1,36 @@
 var validador = document.getElementById('validador');
 
-function validar (){
+function validar() {
     var nome = validador.nome.value;
     var email = validador.email.value;
     var mensagem = validador.mensagem.value;
 
-    if(nome ==""){
+    if (nome == "") {
         alert('Preencha o campo nome!');
         validador.nome.focus();
         return false;
     }
 
-    if(email == "" || email.indexOf('@') == -1 || email.indexOf('.') == -1 ){
+    if (email == "" || email.indexOf('@') == -1 || email.indexOf('.') == -1) {
         alert('Preencha corretamente o campo email!');
         validador.nome.focus();
         return false;
     }
 
-    if(mensagem == "" || mensagem.length <15 ){
-        alert('O campo da mensagem deve possuir no mínimo 15 caracteres, preencha corretamente!');
+    if (mensagem == "" || mensagem.length < 15) {
+        alert('O campo da mensagem deve possuir no mínimo 15 caracteres. Preencha corretamente!');
         validador.nome.focus();
         return false;
     }
-    alert('Sua mensagem foi enviada corretamente e em breve será respondida! Muito Obrigado :)')
+    alert('Sua mensagem foi enviada com sucesso e em breve será respondida! Muito Obrigado :)')
+}
+
+function countText(campodig) {
+    var limite = 300;
+    var campo = campodig.value.length;
+    var maxlength = document.getElementById('mensagem').setAttribute('maxLength', limite);
+    var msg = document.getElementById('msg');
+    var resto = limite - campo;
+    msg.innerHTML = (campo + "/" + limite);
 
 }
